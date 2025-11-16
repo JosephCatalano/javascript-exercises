8104d192e8328fa191ee6c81142b26403d462201
Joseph <joseph1catalano@gmail.com>
2025-11-16 15:19:53 -0500

    Completed 08_removerFromArray exercise: Looped through an array and removed every element equal to the user inputed number.

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, num) {
+  for (let i = array.length; i >= 0; i--) {
+    if (array[i] == num) {
+      array.splice(i, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
