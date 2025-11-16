ea77052adc916840f95b68765a9b9f254161f80a
Joseph <joseph1catalano@gmail.com>
2025-11-16 14:35:15 -0500

    Completed 06-repeatString: Created a function that repeats the string 'x' amount of times.  Function also check if number is not a negative.

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  let newstring = "";
+  if (num >= 0) {
+    for (let i = 0; i <= num - 1; i++) {
+      newstring = string + newstring;
+    }
+    return newstring;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
