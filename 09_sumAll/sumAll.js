09af1e1c920ebb29cd28827f68c9fdc441c5b139
Joseph <joseph1catalano@gmail.com>
2025-11-16 16:10:38 -0500

    Completed 09_sumAll exercise: Looped through a number range starting from the lowest number and finishing at the highest number that was inputed by user. Checked if inputs were integers and greater than 0. Returned the total sum of all the numbers within the given range.

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+  let totalSum = 0;
+  let highestNum, lowestNum;
+  if (first > second) {
+    highestNum = first;
+    lowestNum = second;
+  } else {
+    lowestNum = first;
+    highestNum = second;
+  }
 
+  if (
+    (lowestNum > 0) &
+    (highestNum > 0) &
+    Number.isInteger(lowestNum) &
+    Number.isInteger(highestNum)
+  ) {
+    for (let i = lowestNum; i <= highestNum; i++) {
+      totalSum += i;
+    }
+    return totalSum;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
